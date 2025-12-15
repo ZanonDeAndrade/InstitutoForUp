787fc0f5a1402391d77a7930434e729dc4cefc39
Arthur Zanon de Andrade <zanondea@gmail.com>
2025-12-15 01:46:04 -0300

    Ajuste Curso Valores Humanos

---
+++ server/dist/constants/pillars.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PILLAR_BY_COURSE = exports.PILLAR_IDS = void 0;
+exports.PILLAR_IDS = [
+    "valores-humanos",
+    "lideranca-resultado",
+    "criatividade-empresarial",
+];
+exports.PILLAR_BY_COURSE = {
+    "valores-humanos": "valores-humanos",
+    "cafe-cultural": "valores-humanos",
+    "criterios-valores": "valores-humanos",
+    "desenvolvimento-jovem-lider": "lideranca-resultado",
+    "performando-lideranca-resultado": "lideranca-resultado",
+    "performando-liderancas": "lideranca-resultado",
+    "jovens-lideres": "lideranca-resultado",
+    "jornada-lideristica": "lideranca-resultado",
+    "criatividade-empresarial": "criatividade-empresarial",
+};
